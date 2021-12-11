bd00ecde5702ab92cc54cbf5b486d4c165c5687f
Bruno Costales Aros <83964004+BrunoCostales@users.noreply.github.com>
2021-12-11 04:33:48 -0300

    Add files via upload

---
+++ calculadora.js
@@ -0,0 +1,10 @@
+let sumar= require ('./operadores/sumar')
+let restar= require ('./operadores/restar')
+let dividir= require ('./operadores/dividir')
+let multiplicar= require ('./operadores/multiplicar')
+
+
+console.log(sumar (5,3));
+console.log(restar (5,3));
+console.log(dividir (0,0));
+console.log(multiplicar (5,2)); 
